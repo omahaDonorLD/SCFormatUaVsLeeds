6defea56f7ecaae797f6d52ffda6d4b23f893eb6
my_username <me@email.com>
2018-04-11 03:08:12 +0200

    connectivity ok, kmeans not yet, SCP not yet

--- probingData/graphsTest.cpp
+++ probingData/graphsTest.cpp
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <string>
 #include <math.h>
@@ -23,8 +24,8 @@ typedef struct sln{
 							of elements in list "uavs" so that one can finds the coordinates of uavs[i] */
 	int* counters;// number of elements (size : n_uavs) covered by the uav
 	// double** distances;// squared matrix of distances between uavs
-	igraph_matrix_t dists;// squared matrix of distances between uavs. Type igraph to save space
-	igraph_t gr;// corresponding graph of the solution. Needed for connectivity checking
+	double** dists;// squared matrix of distances between uavs. Type igraph to save space
+	igraph_t gr;// graph of the solution. Needed for connectivity checking
 	int n_uavs;// number of nodes in network : either nodes are uavs (sln : set of uavs), or ground nodes (uav : set of ground nodes)
 }sln;
 
@@ -41,7 +42,7 @@ double bound_2;
 
 void readData(char** argv);
 double euclDistance(double *node1, double *node2);
-void translate(sln* net);
+void translate(sln* net, double threshold);
 void updateDistMat(sln* net, double range);
 
 void readData(char** argv)
@@ -110,9 +111,8 @@ void updateDistMat(sln* net, double range)
 		for(j=i+1;j<=net->n_uavs;j++)
 		{
 			dist=euclDistance(net->uavs[i],net->uavs[j]);
-			dist=( dist > range ? 0 : dist );
-			MATRIX(net->dists, i, j)=dist;
-			MATRIX(net->dists, j, i)=dist;
+			net->dists[i][j]=dist;
+			net->dists[j][i]=dist;
 		}
 }
 
@@ -130,7 +130,7 @@ sln* method1ePasse(double** input_data, int size_input, double threshold)
 	res->counters=(int*)calloc((size_input+1),sizeof(int));
 	res->uavs=(double**)malloc((size_input+1)*sizeof(double*));/* Convenient to keep as whole size, makes no need to reallocate each time a uav is removed/added */
 	double** cl=(double**)malloc((size_input+1)*sizeof(double*));/* temporary variables, contains sum of elements in cluster */
-	igraph_matrix_init(&res->dists,(size_input+1),(size_input+1));
+	res->dists=(double**)malloc((size_input+1)*sizeof(double*));/* temporary variables, contains sum of elements in cluster */
 // 	if k<=max_uavs_avail and there are less uavs than needed then -> segmentation fault. Safety:distances** squared matrix  (size_input+1)*(size_input+1)
 //	for(k=0;k<=size_input;k++)
 //		res->distances[k]=(double*)calloc((size_input+1),sizeof(double));
@@ -142,6 +142,7 @@ sln* method1ePasse(double** input_data, int size_input, double threshold)
 		res->uavs[k]=(double*)calloc(dim,sizeof(double));
 		for(j=0;j<dim;j++)	res->uavs[k][j]=-1;// if uavs coords is (-1,-1), then not deployed
 		cl[k]=(double*)calloc(dim,sizeof(double));
+		for(j=0;j<dim;j++)	res->dists[k]=(double*)calloc((size_input+1),sizeof(double));
 	}
 
 	/* Init first cluster with first element */
@@ -197,7 +198,7 @@ sln* method1ePasse(double** input_data, int size_input, double threshold)
 		}
 	}
 
-	updateDistMat(res, threshold*2);
+	updateDistMat(res, threshold);
 
 	/* Housekeeping */
 	for(k=0;k<=size_input;k++)
@@ -210,15 +211,184 @@ sln* method1ePasse(double** input_data, int size_input, double threshold)
 };
 
 
-void translate(sln* net)
+void k_means(double** data, int n, sln* clusts, double error_tolerance, double range)
 {
-	int i=0,j=0;
 
-	// create general weighted graph, size : n_grnds^2
-	if ( igraph_weighted_adjacency(&net->gr, &net->dists, IGRAPH_ADJ_MAX, NULL, 1) == IGRAPH_NONSQUARE )
-			printf(" Something went wrong for graph init, failed %s, %d, %s \n", __FILE__, __LINE__, __FUNCTION__);
+    int h, i, j; /* loop counters, of course :) */
 
-	// write into file coordinates of uavs
+    double old_error, error = DBL_MAX; /* sum of squared euclidean distance. DBL_MAX : macro defines the maximum finite floating-point value : 1E+37*/
+    double** c1=(double**) malloc((clusts->n_uavs+1)*sizeof(double*)); /* temp centroids */
+
+    assert(data && clusts->n_uavs > 0 && clusts->n_uavs <= n && error_tolerance >= 0); /* for debugging */
+
+    /* initialization : start with given centroids */
+
+     // c1 : temp centroids, clusts->uavs : true centroids
+	for (i=0; i<=clusts->n_uavs; i++)
+		c1[i]=(double*)calloc(dim, sizeof(double));
+
+	double min_distance = DBL_MAX, distance = 0;
+
+    /** Kmeans loop */
+    do {
+        /* save error from last step */
+		old_error = error;
+        error = 0;
+
+        /* clear old counts and temp centroids */
+        for (i=1; i <=clusts->n_uavs; clusts->counters[i++] = 0)
+        {
+			// reinit temp centroids
+            for (j=0;j<dim;c1[i][j++] = 0);
+        }
+
+        for (h = 1; h <= n; h++)
+        {
+            /* Find closest cluster */
+            min_distance = DBL_MAX;
+            for (i=1; i<=clusts->n_uavs; i++)
+            {
+				distance=0;
+				/* considered distance : euclidian squared or squared residuals */
+                for(j=0;j<dim;j++)	distance+=pow(data[h][j]-clusts->uavs[i][j],2);
+                if (distance < min_distance)
+                {
+					min_distance=distance;// find the closest cluster
+                    clusts->labels[h] = i;
+                }
+            }
+            /* update size and temp centroid of the destination cluster */
+			for(j=0;j<dim;j++)	c1[clusts->labels[h]][j] += data[h][j];
+			clusts->counters[clusts->labels[h]]++;
+            /* update standard error */
+            error += min_distance;
+        }
+
+		/* update all centroids */
+        for (i=1; i<=clusts->n_uavs; i++)
+            for (j=0; j<dim; j++)
+                clusts->uavs[i][j] = clusts->counters[i] ? c1[i][j]/clusts->counters[i] : c1[i][j];
+
+    } while (fabs(error - old_error) > error_tolerance);/* if for each iteration, the number of changes made are not different from previous */
+
+
+
+	updateDistMat(clusts, range);
+
+	/* housekeeping */
+	for(h=0;h<=clusts->n_uavs;h++)	free(c1[h]);
+	free(c1);
+
+}
+
+
+void translate(sln* net, double threshold)
+{// note : computing diameter of graph (O(n*|E|)) cost more than comparing each nodes (O(n^2)) : Complete graph : n*|E| = n*sum_i_in{1...n-1}(i) > O(n^2) for n>3
+
+	long int i=0,j=0;
+	igraph_integer_t ncomps=-1;// numbers of connected components
+	igraph_vector_t labels, compssizes;// labels of connected components for each vertex
+	double min_distance=DBL_MAX;// distance of two closest nodes but out of range
+	double current_distance;// used to place a new uav, "shared" is the degree of "merging" of two uavs where the area is shared
+	double shared=threshold/4;
+	int n1=-1,n2=-1;//two closest but out of range nodes
+
+	// here vector "labels" only used to create graph
+	if(igraph_empty(&net->gr, net->n_uavs+1, IGRAPH_UNDIRECTED)==IGRAPH_EINVAL)
+		printf(" Invalid number of vertices, graph init failed %s, %d, %s \n", __FILE__, __LINE__, __FUNCTION__);
+
+	// add edges to graph, edges : two uavs in range
+	for(i=1;i<=net->n_uavs;i++)
+		for(j=i+1;j<=net->n_uavs;j++)
+			if(net->dists[i][j]<2*threshold-shared)
+{
+printf("(%ld,%ld)\t",i,j);
+				igraph_add_edge(&net->gr, i, j);
+}
+printf("end init graph with %d uavs\n",net->n_uavs);
+
+//int k=0;
+	do
+	{
+		
+		if(igraph_vector_init(&labels, 0)==IGRAPH_ENOMEM || igraph_vector_init(&compssizes, 0)==IGRAPH_ENOMEM)
+			printf(" Memory issues, vector init failed %s, %d, %s \n", __FILE__, __LINE__, __FUNCTION__);
+			
+		// compute number of connected components
+		igraph_clusters(&net->gr, &labels, &compssizes, &ncomps, IGRAPH_WEAK);
+printf("start labels\n");
+for(i=0;i<igraph_vcount(&net->gr);i++)	printf("l[%li]:%li\t",i,(long int)VECTOR(labels)[i]);
+printf("end labels\n");
+		// free first used memory since initiated again later
+		igraph_vector_destroy(&compssizes);
+
+		// if graph not one unique connected component then update matrix of distance with new uav (2 as exist isolated vertex 0)
+		if(ncomps>2)
+		{//add new node to reach one connected component
+			min_distance=DBL_MAX;
+			// find two closest but out of range nodes
+			for (i=1;i<=net->n_uavs;i++)
+			{
+				for (j=i+1;j<=net->n_uavs;j++)
+				{
+					/* Find closest clusters but over range */
+					current_distance=net->dists[i][j];
+					if(current_distance<threshold)// skip, in range
+						continue;
+					// check only if different connected component
+					if(current_distance<min_distance && VECTOR(labels)[i] != VECTOR(labels)[j])
+					{// keep two nodes
+						min_distance=current_distance;
+						n1=i;
+						n2=j;
+					}
+				}
+			}
+
+			// free vector of labels
+			igraph_vector_destroy(&labels);
+
+			if(n1<0 || n2<0)
+			{
+				printf(" Something went wrong, adding new uav will fail %s, %d, %s \n", __FILE__, __LINE__, __FUNCTION__);
+				return;
+			}
+
+			// assign coordinates of new created node
+			net->n_uavs++;
+			// assign coordinates
+			for (j=0;j<dim;j++)	net->uavs[net->n_uavs][j]=(net->uavs[n1][j]+net->uavs[n2][j])/2;
+			// increase number of elements in new cluster
+			net->counters[net->n_uavs]++;// To 1
+			// extend size of graph
+			igraph_add_vertices(&net->gr, 1, 0);
+			// update new distances
+			updateDistMat(net, threshold);
+			// create links where needed
+			if(net->dists[net->n_uavs][n1]<2*threshold-shared)
+{
+printf("(%d,%d)\n",net->n_uavs, n1);
+				igraph_add_edge(&net->gr, net->n_uavs, n1);
+}
+			if(net->dists[net->n_uavs][n2]<2*threshold-shared)
+{
+printf("(%d,%d)\n",net->n_uavs, n2);
+				igraph_add_edge(&net->gr, net->n_uavs, n2);
+}
+printf("1. dist n_uav-n1 : %lf, thresh : %lf, nuav : %d, n1 : %d\n", net->dists[net->n_uavs][n1], 2*threshold-shared, net->n_uavs, n1);
+printf("2. dist n_uav-n2 : %lf, thresh : %lf, nuav : %d, n2 : %d\n", net->dists[net->n_uavs][n2], 2*threshold-shared, net->n_uavs, n2);
+printf(" %d and %d coords (%lf,%lf) (%lf,%lf)\n",n1,n2, net->uavs[n1][0], net->uavs[n1][1], net->uavs[n2][0], net->uavs[n2][1]);
+printf("dist n1-n2 after update %d - %d\n",(int)net->dists[n2][n1],(int)net->dists[n1][n2]);
+
+//printf(" %d and %d coords (%lf,%lf) (%lf,%lf)\n",n1,n2, net->uavs[n1][0], net->uavs[n1][1], net->uavs[n2][0], net->uavs[n2][1]);
+//printf("with %li vertices, and new uav %d coords (%lf,%lf)\n", (long int)igraph_vcount(&net->gr), net->n_uavs, net->uavs[net->n_uavs][0], net->uavs[net->n_uavs][1]);
+//printf(" vcount %li ecount %li\n", (long int)igraph_vcount(&net->gr), (long int)igraph_ecount(&net->gr));
+		}
+//	}while(ncomps>2 && k++ < 5);
+	}while(ncomps>2);
+
+
+	// write graph into file
 	FILE* fp;
 	fp=fopen("graphs.csv","w");
 	igraph_vector_t ite_edgs;
@@ -226,109 +396,24 @@ void translate(sln* net)
 	igraph_get_edgelist(&net->gr, &ite_edgs, 0);
 	int n=igraph_ecount(&net->gr);
 	long int buff1,buff2;// needed for the cast
-	double weight;
 	for (i=0, j=0; j<n; i+=2, j++)
 	{
 		buff1=VECTOR(ite_edgs)[i];
 		buff2=VECTOR(ite_edgs)[i+1];
-		weight=EAN(&net->gr, "weight", j);
 		fprintf(fp,"%lf,%lf\n", net->uavs[buff1][0], net->uavs[buff1][1]);
 		fprintf(fp,"%lf,%lf\n", net->uavs[buff2][0], net->uavs[buff2][1]);
 		fprintf(fp,"\n");
-//printf("%ld,%ld,%lf\n",buff1,buff2,weight);
+printf("(%ld,%ld)\t",buff1,buff2);
 //fprintf(bufffp,"%ld-%ld:%lf,%lf:%lf,%lf:W:%lf\n", buff1, buff2, net->uavs[buff1][0], net->uavs[buff1][1], net->uavs[buff2][0], net->uavs[buff2][1],weight);
 	}
+printf("\n");
 	fclose(fp);
 	igraph_vector_destroy(&ite_edgs);
-
-	// subgraph, true size : (1,...,n_uavs)^2
-	igraph_t true_gr;
-	igraph_vs_t vs;
-	igraph_vs_seq(&vs, 0, net->n_uavs);
-	// i here is used as just a buffer
-	i=igraph_induced_subgraph(&net->gr, &true_gr, vs, IGRAPH_SUBGRAPH_COPY_AND_DELETE);
-	if(i==IGRAPH_ENOMEM || i==IGRAPH_EINVVID)
-		printf(" %s issue, induced subgraph failed %s, %d, %s \n", ( i==IGRAPH_ENOMEM ? "IGRAPH_ENOMEM": "IGRAPH_EINVVID"), __FILE__, __LINE__, __FUNCTION__);
-
-	// gets labels of connected components of network
-	igraph_vector_t labels, compssizes;
-	igraph_integer_t ncomps=0;
-	if(igraph_vector_init(&labels, 0)==IGRAPH_ENOMEM || igraph_vector_init(&compssizes, 0)==IGRAPH_ENOMEM)
-		printf(" Memory issues, vector init failed %s, %d, %s \n", __FILE__, __LINE__, __FUNCTION__);
-	igraph_clusters(&true_gr, &labels, &compssizes, &ncomps, IGRAPH_WEAK);
-printf("NUMBER OF COMPONENTS %li\n", (long int)ncomps);
-
-	// write connected components coordinates into files
-	FILE* nfp[nbr_grnds+1];
-	char name[100];
-	for(i=0;i<ncomps;i++)
-	{
-		sprintf(name,"comp-%d.csv",i);
-		nfp[i]=fopen(name,"w");
-	}
-	for(i=1;i<=net->n_uavs;i++)
-	{
-		buff1=(long int)VECTOR(labels)[i];
-		for (j=0;j<dim;j++)
-		{
-			// skip comma missing for last dim
-			if(j==dim-1)	fprintf(nfp[buff1],"%lf\n", net->uavs[i][j]);
-			else fprintf(nfp[buff1],"%lf,", net->uavs[i][j]);
-		}
-	}
-	for(i=0;i<ncomps;i++)
-		fclose(nfp[i]);
-
-/*
-	fp=fopen("components.csv","w");
-	igraph_vector_ptr_t compolist;
-	if(igraph_vector_ptr_init(&compolist, 0)==IGRAPH_ENOMEM)
-		printf(" Memory issues, vector init failed %s, %d, %s \n", __FILE__, __LINE__, __FUNCTION__);
-	igraph_decompose(&true_gr, &compolist, IGRAPH_WEAK, -1, 0);
-	igraph_write_graph_edgelist((igraph_t*)VECTOR(compolist)[0], fp);
-	fclose(fp);
-
-	for (i=0; i<igraph_vector_ptr_size(&compolist); i++)
-	{
-		igraph_destroy(VECTOR(compolist)[i]);
-		free(VECTOR(compolist)[i]);
-	}
-*/
-
-/*
-	igraph_vector_ptr_t comps_list;
-	igraph_vector_ptr_init(&comps_list, 0);// list of components first need to be initiated before call to decompose
-*/
-	/* 1) -1 : maxcompno, maximum number of components to return. -1 if nolimit
-	 * 2)  2 : minelements, minimum number of vertices a component contains to be placed in the components vector. Here 2 skips isolated vertices */
-/*
-	int someintdontknowyetwhatfor=igraph_decompose(&gr, &comps_list,IGRAPH_WEAK, -1, 2);
-	for(i=0;i<igraph_vector_ptr_size(&comps_list);i++)
-	{
-		igraph_t *buff=VECTOR(comps_list)[i];
-		igraph_es_t es;
-		igraph_es_all(buff, &es);
-		while (!igraph_es_end(buff, &es))
-		{
-			igraph_real_t *from, *to;
-			igraph_get_vertex_attribute(buff, "id", igraph_es_from(buff, &es), (void**) &from, 0);
-			igraph_get_vertex_attribute(buff, "id", igraph_es_to(buff, &es), (void**) &to, 0);
-			printf("%li %li\n", (long int) *from, (long int) *to);
-			igraph_es_next(buff, &es);
-		}
-	}
-*/
-//	free_complist(&complist); */
-//	igraph_destroy(&true_gr);
-//	igraph_vector_destroy(&edgs);
-
-	/* Housekeeping */
 };
 
 void freeSln(sln* free_sln)
 {
 	igraph_destroy(&free_sln->gr);
-	igraph_matrix_destroy(&free_sln->dists);
 	free(free_sln);
 };
 
@@ -344,12 +429,14 @@ int main(int argc, char** argv)
 	bound_1=1000;
 	bound_2=1000;
 
-	double threshold=uavs_range/2;
+	double radius=uavs_range/4;
 
-	sln *res=method1ePasse(grnds, nbr_grnds, threshold);
+	sln *res=method1ePasse(grnds, nbr_grnds, radius);
 
 	int i=0,j=0;
+	translate(res, radius);
 
+printf("rl.csv nuavs : %d ",res->n_uavs);
 	FILE* fp;
 	fp=fopen("rl.csv","w");
 	for(i=1;i<=res->n_uavs;i++)
@@ -361,9 +448,22 @@ int main(int argc, char** argv)
 		}
 	fclose(fp);
 
-	translate(res);
+/*
+	k_means(grnds, nbr_grnds, res, 0.0001, radius);
+
+	fp=fopen("rlkmeans.csv","w");
+	for(i=1;i<=res->n_uavs;i++)
+		for (j=0;j<dim;j++)
+		{
+			// skip comma missing for last dim
+			if(j==dim-1)	fprintf(fp,"%lf\n", res->uavs[i][j]);
+			else fprintf(fp,"%lf,", res->uavs[i][j]);
+		}
+	fclose(fp);
+*/
+//	translate(res, radius);
 //	igraph_t graph_sol=translate(res);
 
 
-	printf("%lf\n",grnds[17][0]);
+	printf("n uvas : %d and grnds[17][0] : %lf\n", res->n_uavs,grnds[17][0]);
 }
